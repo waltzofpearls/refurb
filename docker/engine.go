c109825f5e1ee4b0fd23411723536c43e0c5986d
Rollie Ma <rollie.ma@gmail.com>
2018-08-08 01:30:48 -0700

    WIP

--- docker/engine.go
+++ docker/engine.go
@@ -1,6 +1,16 @@
 package docker
 
-import "github.com/docker/docker/client"
+import (
+	"context"
+	"io"
+	"io/ioutil"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+)
+
+var startOpts = types.ContainerStartOptions{}
 
 type Engine struct {
 	client *client.Client
@@ -14,4 +24,58 @@ func New() (*Engine, error) {
 	return &Engine{
 		client: c,
 	}, nil
+}
+
+func (e *Engine) Run(ctx context.Context, proc *Process) error {
+	config := toConfig(proc)
+	hostConfig := toHostConfig(proc)
+
+	// create pull options with encoded authorization credentials.
+	pullopts := types.ImagePullOptions{}
+	if proc.AuthConfig.Username != "" && proc.AuthConfig.Password != "" {
+		pullopts.RegistryAuth, _ = encodeAuthToBase64(proc.AuthConfig)
+	}
+
+	// automatically pull the latest version of the image if requested
+	// by the process configuration.
+	if proc.Pull {
+		rc, perr := e.client.ImagePull(ctx, config.Image, pullopts)
+		if perr == nil {
+			io.Copy(ioutil.Discard, rc)
+			rc.Close()
+		}
+		if perr != nil && proc.AuthConfig.Password != "" {
+			return perr
+		}
+	}
+
+	_, err := e.client.ContainerCreate(ctx, config, hostConfig, nil, proc.Name)
+	if client.IsErrImageNotFound(err) {
+		// automatically pull and try to re-create the image if the
+		// failure is caused because the image does not exist.
+		rc, perr := e.client.ImagePull(ctx, config.Image, pullopts)
+		if perr != nil {
+			return perr
+		}
+		io.Copy(ioutil.Discard, rc)
+		rc.Close()
+
+		_, err = e.client.ContainerCreate(ctx, config, hostConfig, nil, proc.Name)
+	}
+	if err != nil {
+		return err
+	}
+
+	if len(proc.NetworkMode) == 0 {
+		for _, net := range proc.Networks {
+			err = e.client.NetworkConnect(ctx, net.Name, proc.Name, &network.EndpointSettings{
+				Aliases: net.Aliases,
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return e.client.ContainerStart(ctx, proc.Name, startOpts)
 }
